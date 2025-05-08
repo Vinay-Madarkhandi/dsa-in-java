0283f4c72d21ae64db0b4ae9078925082f056a9e
Madarkhandi Vinay Shantappa <vinaymadarkhandi2022@gmail.com>
2025-05-08 19:54:46 +0530

    08-05-2025

--- Bit Manipulation/BitOperations.java
+++ Bit Manipulation/BitOperations.java
@@ -29,9 +29,58 @@ class BitOperations {
         return no | bitMask;
     }
 
+    public static int clearIBits(int n, int i) {
+        int bitMask = ~(0) << i;
+        return (n & bitMask);
+    }
+
+    public static int clearRangeinBits(int n, int i, int j) {
+        int a = (~0) << (j + 1);
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    // Fast exponentiation is a concept where we find the a^n solution is best
+    // optiomal time using bit operators in the O(log n) time complexity.
+    // similar to this there is another concept called modular exponentation where
+    // (a^n % x) is find in best optimal solution
+
+    public static int fastExponentaion(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println(getIthBit(10, 2));
         System.out.println(setIthBit(10, 2));
         System.out.println(clearIthBit(10, 1));
+        System.out.println(clearIBits(10, 2));
+        System.out.println(clearRangeinBits(10, 2, 7));
+        System.out.println(isPowerOfTwo(32));
+        System.out.println(countSetBits(10));
+        System.out.println(fastExponentaion(3, 5));
     }
 }
